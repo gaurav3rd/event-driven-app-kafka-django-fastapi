51f0a148ddc603db072eca68022ab554df452052
Gaurav Jaiswal <jr.gaurav2015@gmail.com>
2024-10-19 12:29:32 +0545

    update docker script

--- services/consumer/.scripts/start.sh
+++ services/consumer/.scripts/start.sh
@@ -3,6 +3,8 @@
 echo "initializing container..."
 python manage.py runserver 0.0.0.0:20001
 
+celery -A consumer worker --beat --loglevel=debug
+
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
