a6bc6933ffe24352c84dfba4f26bc951900f1f8a
Gaurav Jaiswal <jr.gaurav2015@gmail.com>
2024-10-30 22:08:58 +0545

    minor refactor

--- services/consumer/config/kafka_provider.py
+++ services/consumer/config/kafka_provider.py
@@ -12,8 +12,8 @@ def get_kafka_consumer(topic: str, **kwargs) -> KafkaConsumer:
     return KafkaConsumer(
         topic,
         bootstrap_servers=["kafka:9093"],
-        auto_offset_reset="earliest",
         group_id="todo",
+        auto_offset_reset="earliest",
         enable_auto_commit=True,
         value_deserializer=lambda x: json.loads(x.decode("utf-8")),
         **kwargs
